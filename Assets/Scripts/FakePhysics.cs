b9376e695e910ece6e5bee3e9a413a926abce9b1
Richard Leinfellner <richard@modulo17.com>
2018-10-02 16:00:36 +0100

    Ready for Lecture 1

--- Assets/Scripts/FakePhysics.cs
+++ Assets/Scripts/FakePhysics.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class FakePhysics : MonoBehaviour {
 
-
     private Rigidbody2D mRB2;       //We dont want other classes messing with this, as we control Physics
 
     private SpriteRenderer mSR;     //May need this later
@@ -17,6 +16,7 @@ public class FakePhysics : MonoBehaviour {
     [SerializeField]
     private float RotationSpeed = 360.0f;
 
+    private Vector3 mVelocity = Vector3.zero;
 
     // Use this for initialization
     void Start () {
@@ -32,14 +32,41 @@ public class FakePhysics : MonoBehaviour {
     }
 	
 	// Update is called once per frame
-	void FixedUpdate () {
-        float tMoveSpeed = Input.GetAxis("Vertical") * Speed;
-        float tRotationSpeed = Input.GetAxis("Horizontal") * RotationSpeed;
-        transform.Rotate(0, 0, tRotationSpeed * RotationSpeed * Time.deltaTime);
-        Vector3 tMoveVector = Quaternion.Euler(0, 0, transform.rotation.z) * (Vector3.up * tMoveSpeed);
-        transform.position += tMoveVector * Time.deltaTime;
-        Debug.Log(tMoveVector);
+	void Update () {
+        Vector3 tNewPostion;        //Storage for new position
+        DoMove();
+        if(DoWrap(out tNewPostion)) {       //Check if we should wrap
+            transform.position = tNewPostion;   //Yes we need to have new position
+        }
+    }
+
+    void DoMove() {
+        float tThrust = Input.GetAxis("Vertical") * Speed;       //Get Thrust
+        float tRotate = Input.GetAxis("Horizontal") * RotationSpeed; //Get Rotation
+        transform.Rotate(0, 0, tRotate * RotationSpeed * Time.deltaTime);    //Rotate ship
+        mVelocity += Quaternion.Euler(0, 0, transform.rotation.z) * transform.up * tThrust * Speed; //Non mass velocity
+        transform.position += mVelocity * Time.deltaTime; //Work out new position
+    }
+
+    bool    DoWrap(out Vector3 vNewPosition) {
+        float   tHeight = Camera.main.orthographicSize;  //Figure out what Camera can see
+        float   tWidth = Camera.main.aspect * tHeight;  //Use aspect ratio to work out Width
+        bool    tMoved = false;     //Default is no wrap
+        vNewPosition = transform.position;
+        if (vNewPosition.x > tWidth) {
+            vNewPosition.x -= 2.0f * tWidth;     //If out of bounds reset position
+            tMoved = true;      //We are wrapping
+        } else if (vNewPosition.x < -tWidth) {
+            vNewPosition.x += 2.0f * tWidth;
+            tMoved = true;
+        }
+        if (vNewPosition.y > tHeight) {
+            vNewPosition.y -= 2.0f * tHeight;
+            tMoved = true;
+        } else if (vNewPosition.y < -tHeight) {
+            vNewPosition.y += 2.0f * tHeight;
+            tMoved = true;
+        }
+        return tMoved;
     }
 }
-
-
