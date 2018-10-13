5c6475ce2342bdae152c6a1ed5ab536e38d85a18
Richard Leinfellner <richard@modulo17.com>
2018-10-13 17:13:54 +0100

    Fixed Missing fire code, lost in merge

--- Assets/Scripts/PlayerShip.cs
+++ Assets/Scripts/PlayerShip.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PlayerShip : FakePhysics {
 
+    [SerializeField]
+    private Transform GunPosition;
+
+
     //The rock just moves by simply adding the velocity & rotation
     protected override void DoMove() {
         float tThrust = Input.GetAxis("Vertical") * Speed;       //Get Thrust
@@ -12,5 +16,11 @@ public class PlayerShip : FakePhysics {
         mVelocity += Quaternion.Euler(0, 0, transform.rotation.z) * transform.up * tThrust * Speed; //Non mass velocity
         mVelocity = ClampedVelocity();
         base.DoMove();  //Call Base class which will apply Velocity
+        DoFiring();
+    }
+    void DoFiring() {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            GM.CreateBullet(GunPosition.position, transform.up);
+        }
     }
 }
